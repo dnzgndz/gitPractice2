92f55edce505664748106f55db25a69186b00ade
Deniz Gunduz <64120602+dnzgndz@users.noreply.github.com>
2020-04-28 15:54:52 -0400

    Xine
    how is this life going to be ?

--- src/day_01/HappyBirthday.java
+++ src/day_01/HappyBirthday.java
@@ -3,5 +3,13 @@ package day_01;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("DENIZ GUNDUZ");
+        
+        
+        
+        
+        System.out.println("how is this life going to be);
+        
+        
+        
     }
 }
